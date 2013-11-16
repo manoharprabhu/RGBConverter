17a9ddb2e34c21e9a717908f76f97b572235f98e
manoharprabhu <manoharagent13@gmail.com>
2013-11-16 23:14:43 +0530

    Removed convert button. Added sliders for RGB. conversion happens on change of RGB slider or text box.

--- gen/com/example/flat/R.java
+++ gen/com/example/flat/R.java
@@ -29,18 +29,23 @@ public final class R {
         public static final int green=0x7f020005;
         public static final int green_button=0x7f020006;
         public static final int green_dark=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int red=0x7f020009;
-        public static final int red_button=0x7f02000a;
-        public static final int red_dark=0x7f02000b;
+        public static final int hexlabel=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int red=0x7f02000a;
+        public static final int red_button=0x7f02000b;
+        public static final int red_dark=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int blueB=0x7f080003;
-        public static final int greenG=0x7f080002;
-        public static final int hexValue=0x7f080004;
+        public static final int action_settings=0x7f080009;
+        public static final int blueB=0x7f080006;
+        public static final int blueSeekBar=0x7f080005;
+        public static final int colorIndicator=0x7f080008;
+        public static final int greenG=0x7f080004;
+        public static final int greenSeekBar=0x7f080003;
+        public static final int hexValue=0x7f080007;
         public static final int linearLayout=0x7f080000;
-        public static final int redR=0x7f080001;
+        public static final int redR=0x7f080002;
+        public static final int redSeekBar=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/flat/MainActivity.java
+++ src/com/example/flat/MainActivity.java
@@ -9,10 +9,16 @@ import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,9 +29,166 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		setListeners();
 		
 	}
 	
+	public void setListeners(){
+		OnSeekBarChangeListener blueListener = new OnSeekBarChangeListener() {
+			
+	
+			@Override
+			public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+				((EditText)findViewById(R.id.blueB)).setText(String.valueOf(arg0.getProgress()));
+				convertHelper();
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		
+		OnSeekBarChangeListener greenListener = new OnSeekBarChangeListener() {
+			
+			
+			@Override
+			public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+				((EditText)findViewById(R.id.greenG)).setText(String.valueOf(arg0.getProgress()));
+				convertHelper();
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		
+		OnSeekBarChangeListener redListener = new OnSeekBarChangeListener() {
+			
+			
+			@Override
+			public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+				((EditText)findViewById(R.id.redR)).setText(String.valueOf(arg0.getProgress()));
+				convertHelper();
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		
+		((SeekBar)findViewById(R.id.redSeekBar)).setOnSeekBarChangeListener(redListener);
+		((SeekBar)findViewById(R.id.greenSeekBar)).setOnSeekBarChangeListener(greenListener);
+		((SeekBar)findViewById(R.id.blueSeekBar)).setOnSeekBarChangeListener(blueListener);
+	
+		
+		
+		
+		((EditText)findViewById(R.id.redR)).addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				if(arg0.toString().equals("")){
+					arg0.append('0');
+				}
+				((SeekBar)findViewById(R.id.redSeekBar)).setProgress(Integer.parseInt(arg0.toString()));
+			((EditText)findViewById(R.id.redR)).setSelection(((EditText)findViewById(R.id.redR)).length());
+			convertHelper();
+			}
+		});
+		
+((EditText)findViewById(R.id.greenG)).addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				if(arg0.toString().equals("")){
+					arg0.append('0');
+				}
+				((SeekBar)findViewById(R.id.greenSeekBar)).setProgress(Integer.parseInt(arg0.toString()));
+			((EditText)findViewById(R.id.greenG)).setSelection(((EditText)findViewById(R.id.greenG)).length());
+			convertHelper();
+			}
+		});
+
+
+((EditText)findViewById(R.id.blueB)).addTextChangedListener(new TextWatcher() {
+	
+	@Override
+	public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+			int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void afterTextChanged(Editable arg0) {
+		if(arg0.toString().equals("")){
+			arg0.append('0');
+		}
+		((SeekBar)findViewById(R.id.blueSeekBar)).setProgress(Integer.parseInt(arg0.toString()));
+	((EditText)findViewById(R.id.blueB)).setSelection(((EditText)findViewById(R.id.blueB)).length());
+	convertHelper();
+	}
+});
+
+
+	
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,8 +197,7 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	public void convertToHex(View view) {
-	
+	public void convertHelper(){
 		int red=0,green=0,blue=0;
 
 		if(((EditText)findViewById(R.id.redR)).getText().toString().trim().equals("") == false)
@@ -75,7 +237,8 @@ public class MainActivity extends Activity {
 		TextView hexValueTextView = (TextView)findViewById(R.id.hexValue);
 		
 		hexValueTextView.setText("#" + getHexValue(red) + getHexValue(green) + getHexValue(blue));
-		hexValueTextView.setTextColor(Color.rgb(red, green, blue));
+		((TextView)findViewById(R.id.colorIndicator)).setBackgroundColor(Color.rgb(red, green, blue));
+		hexValueTextView.setTextColor(Color.rgb(0, 0, 0));
 	}
 	
 	public String getHexValue(int value){
